5ad1fd63b0fea40b8a0071baa40977fb83498549
jmakowie <jmakowie@d17-430-pc-02.ipa.ii.agh.edu.pl>
2023-04-16 11:10:39 +0200

    implemented log in interface

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+
+    <div v-if="loggedIn">Witaj {{email}}
+      <button @click="logMeOut()">Wyloguj</button>
+    </div>
+
+    <div v-else>
+    <h1>Witaj w systemie do zapisow na zajecia</h1>
+    <div>Zaloguj sie mailem:
+    <input type="email" v-model="email">
+      <button @click="logMeIn()">Wchodze</button></div>
+    </div>
+
+
   </div>
+
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      email: '',
+      password: '',
+      loggedIn: false
+    };
+  },
+  methods: {
+    logMeIn() {
+        this.loggedIn = true;
+    },
+    logMeOut() {
+      this.loggedIn = false;
+    }
+  }
 }
 </script>
 
